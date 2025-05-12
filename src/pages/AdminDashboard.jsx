276a6526633e0834da6ac4eae9caef876af6cb97
jose-tejaxun <danieltejaxun123@gmail.com>
2025-05-11 18:19:52 -0600

    Delete src directory

--- src/pages/AdminDashboard.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function AdminDashboard() {
-  return <div className="p-8 text-center">Dashboard de Administrador</div>;
-}
--- src/pages/UserDashboard.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function UserDashboard() {
-  return <div className="p-8 text-center">Dashboard de Usuario</div>;
-}
