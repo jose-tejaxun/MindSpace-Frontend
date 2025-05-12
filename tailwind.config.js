e2433ea1c523374c9fcd88f164e6257875ff9a6e
jose-tejaxun <danieltejaxun123@gmail.com>
2025-05-11 18:20:14 -0600

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-    "./src/**/*.{js,jsx,ts,tsx}"
-];
-export const darkMode = 'media';
-export const theme = {
-    extend: {},
-};
-export const plugins = [];
